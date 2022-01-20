4e26cb9237e8544da41efcece7d131a666cdb96b
Chris <chrisgerth010592@gmail.com>
2022-01-19 20:22:44 -0600

    wip integration

--- Core/Src/networking/networking.c
+++ Core/Src/networking/networking.c
@@ -1,4 +1,9 @@
 #include "networking.h"
+#include "FreeRTOS.h"
+#include "FreeRTOS_IP.h"
+#include "FreeRTOS_IP_Private.h"
+#include "NetworkBufferManagement.h"
+#include <string.h>
 
 uint8_t mac[] = { 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
 uint8_t ip[] = { 10, 111, 76, 123 };
@@ -7,77 +12,145 @@ uint8_t ip[] = { 10, 111, 76, 123 };
 #define BUFFER_SIZE 550
 static uint8_t buf[BUFFER_SIZE+1];
 
-void NetworkingInit(SPI_HandleTypeDef *spiHandle) {
-	/* Ethernet Init*/
-	ES_enc28j60SpiInit(spiHandle);
-	ES_enc28j60Init(mac);
+static SPI_HandleTypeDef *spiHandle = NULL;
 
-	uint8_t enc28j60_rev = ES_enc28j60Revision();
 
-	if (enc28j60_rev <= 0) {
-		while (1); //
-		//error(2, 0);
+//////////////////////////////////
+// FreeRTOS/MAC Interface
+
+BaseType_t xNetworkInterfaceInitialise( void )
+{
+	BaseType_t xReturn = pdFAIL;
+
+	if(spiHandle != NULL){
+		ES_enc28j60SpiInit(spiHandle);
+		ES_enc28j60Init(mac);
+
+		uint8_t enc28j60_rev = ES_enc28j60Revision();
+
+		if (enc28j60_rev > 0) {
+			xReturn = pdPASS;
+		}
 	}
 
-	ES_init_ip_arp_udp_tcp(mac, ip, MYWWWPORT);
+    return xReturn;
 }
 
-void ES_PingCallback(void) {
-}
-
-uint16_t http200ok(void)
+BaseType_t xNetworkInterfaceOutput( NetworkBufferDescriptor_t * const pxDescriptor,
+                                    BaseType_t xReleaseAfterSend )
 {
-  return(ES_fill_tcp_data_p(buf,0,PSTR("HTTP/1.0 200 OK\r\nContent-Type: text/html\r\nPragma: no-cache\r\n\r\n")));
+	ES_enc28j60PacketSend(pxDescriptor->xDataLength, pxDescriptor->pucEthernetBuffer );
+
+    iptraceNETWORK_INTERFACE_TRANSMIT();
+
+    if( xReleaseAfterSend != pdFALSE )
+    {
+        vReleaseNetworkBufferAndDescriptor( pxDescriptor );
+    }
+
+    return pdTRUE;
 }
 
-// prepare the webpage by writing the data to the tcp send buffer
-uint16_t print_webpage(uint8_t *buf)
+
+/* The deferred interrupt handler is a standard RTOS task.  FreeRTOS's centralised
+deferred interrupt handling capabilities can also be used. */
+void handlePackets(void)
 {
-  uint16_t plen;
-  plen=http200ok();
-  plen=ES_fill_tcp_data_p(buf,plen,PSTR("<html><head><title>Arduino ENC28J60 Ethernet Shield V1.0</title></head><body>"));
-  plen=ES_fill_tcp_data_p(buf,plen,PSTR("<center><h1>Welcome to Arduino ENC28J60 Ethernet Shield V1.0</h1>"));
-  plen=ES_fill_tcp_data_p(buf,plen,PSTR("<hr><br><h2><font color=\"blue\">-- Put your ARDUINO online -- "));
-  plen=ES_fill_tcp_data_p(buf,plen,PSTR("<br> Control digital outputs"));
-  plen=ES_fill_tcp_data_p(buf,plen,PSTR("<br> Read digital analog inputs HERE"));
-  plen=ES_fill_tcp_data_p(buf,plen,PSTR("<br></font></h2>") );
-  plen=ES_fill_tcp_data_p(buf,plen,PSTR("</center><hr>"));
-  plen=ES_fill_tcp_data_p(buf,plen,PSTR("V1.0 <a href=\"http://blog.thiseldo.co.uk\">blog.thiseldo.co.uk</a>"));
-  plen=ES_fill_tcp_data_p(buf,plen,PSTR("</body></html>"));
+	NetworkBufferDescriptor_t *pxBufferDescriptor;
+	size_t xBytesReceived;
+	/* Used to indicate that xSendEventStructToIPTask() is being called because
+	of an Ethernet receive event. */
+	xIPStackEvent_t xRxEvent;
 
-  return(plen);
+	do {
+
+		xBytesReceived=ES_enc28j60PacketReceive(BUFFER_SIZE, buf);
+
+		if( xBytesReceived > 0 )
+		{
+			/* Allocate a network buffer descriptor that points to a buffer
+			large enough to hold the received frame.  As this is the simple
+			rather than efficient example the received data will just be copied
+			into this buffer. */
+			pxBufferDescriptor = pxGetNetworkBufferWithDescriptor( xBytesReceived, 0 );
+
+			if( pxBufferDescriptor != NULL )
+			{
+				/* pxBufferDescriptor->pucEthernetBuffer now points to an Ethernet
+				buffer large enough to hold the received data.  Copy the
+				received data into pcNetworkBuffer->pucEthernetBuffer.  Here it
+				is assumed ReceiveData() is a peripheral driver function that
+				copies the received data into a buffer passed in as the function's
+				parameter.  Remember! While is is a simple robust technique -
+				it is not efficient.  An example that uses a zero copy technique
+				is provided further down this page. */
+				memcpy( pxBufferDescriptor->pucEthernetBuffer, buf, xBytesReceived );
+				pxBufferDescriptor->xDataLength = xBytesReceived;
+
+				/* See if the data contained in the received Ethernet frame needs
+				to be processed.  NOTE! It is preferable to do this in
+				the interrupt service routine itself, which would remove the need
+				to unblock this task for packets that don't need processing. */
+				if( eConsiderFrameForProcessing( pxBufferDescriptor->pucEthernetBuffer )
+																	  == eProcessBuffer )
+				{
+					/* The event about to be sent to the TCP/IP is an Rx event. */
+					xRxEvent.eEventType = eNetworkRxEvent;
+
+					/* pvData is used to point to the network buffer descriptor that
+					now references the received data. */
+					xRxEvent.pvData = ( void * ) pxBufferDescriptor;
+
+					/* Send the data to the TCP/IP stack. */
+					if( xSendEventStructToIPTask( &xRxEvent, 0 ) == pdFALSE )
+					{
+						/* The buffer could not be sent to the IP task so the buffer
+						must be released. */
+						vReleaseNetworkBufferAndDescriptor( pxBufferDescriptor );
+
+						/* Make a call to the standard trace macro to log the
+						occurrence. */
+						iptraceETHERNET_RX_EVENT_LOST();
+					}
+					else
+					{
+						/* The message was successfully sent to the TCP/IP stack.
+						Call the standard trace macro to log the occurrence. */
+						iptraceNETWORK_INTERFACE_RECEIVE();
+					}
+				}
+				else
+				{
+					/* The Ethernet frame can be dropped, but the Ethernet buffer
+					must be released. */
+					vReleaseNetworkBufferAndDescriptor( pxBufferDescriptor );
+				}
+			}
+			else
+			{
+				/* The event was lost because a network buffer was not available.
+				Call the standard trace macro to log the occurrence. */
+				iptraceETHERNET_RX_EVENT_LOST();
+			}
+		}
+
+	} while(xBytesReceived > 0);
+
 }
 
+
+
+//////////////////////////////////
+// RTOS Tasking Interface
+
+void NetworkingInit(SPI_HandleTypeDef *spiHandle_in) {
+	spiHandle = spiHandle_in;
+}
 
 void NetworkingPeriodic() {
 
-	uint16_t plen, dat_p;
-	dat_p=ES_packetloop_icmp_tcp(buf,ES_enc28j60PacketReceive(BUFFER_SIZE, buf));
-
-    /* dat_p will be unequal to zero if there is a valid
-     * http get */
-    if(dat_p==0){
-      // no http request
-      return;
-    }
-
-
-    // tcp port 80 begin
-    if (strncmp("GET ",(char *)&(buf[dat_p]),4)!=0){
-      // head, post and other methods:
-      dat_p=http200ok();
-      dat_p=ES_fill_tcp_data_p(buf,dat_p,PSTR("<h1>200 OK</h1>"));
-    }
-    // just one web page in the "root directory" of the web server
-    if (strncmp("/ ",(char *)&(buf[dat_p+4]),2)==0){
-      dat_p=print_webpage(buf);
-    }
-    else{
-      dat_p=ES_fill_tcp_data_p(buf,0,PSTR("HTTP/1.0 401 Unauthorized\r\nContent-Type: text/html\r\n\r\n<h1>401 Unauthorized</h1>"));
-    }
-
-    ES_www_server_reply(buf,dat_p); // send web page data
-    // tcp port 80 end
-
+	handlePackets();
 
 }
+
+
